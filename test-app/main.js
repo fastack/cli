fbc7c262fda28eeb16d125e32b83d57001956482
Patrick DeVivo <patrick.devivo@gmail.com>
2015-06-09 16:26:56 -0400

    add some documentation

--- test-app/main.js
+++ test-app/main.js
@@ -2,3 +2,7 @@ var uid = require('matthewmueller/uid');
 
 // alert(uid());
 require('./test.coffee')
+
+// var evens = [2, 4, 6, 8, 10, 12]
+// var odds = evens.map(v => v + 1);
+// console.log(odds)
